a002b02db41d99c94b57e7990ca5c0f1c76555a5
oksana_martyniuk <mjlinkoln@gmail.com>
2026-09-06 16:43:55 +0300

    add authentication

--- src/server.js
+++ src/server.js
@@ -8,6 +8,8 @@ import { connectMongoDB } from './db/connectMongoDB.js';
 import notesRoutes from './routes/notesRoutes.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
+import authRoutes from './routes/authRoutes.js';
+import cookieParser from 'cookie-parser';
 const app = express();
 const PORT = process.env.PORT || 3000;
 const logger = pinoHttp({
@@ -27,7 +29,9 @@ const logger = pinoHttp({
 app.use(logger);
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 app.use(notesRoutes);
+app.use('/auth', authRoutes);
 app.use(notFoundHandler);
 app.use(errors());
 app.use(errorHandler);
