b228d9a1ab1a71f2a0c217068bba3942a52bc553
oksana_martyniuk <mjlinkoln@gmail.com>
2026-09-08 21:22:50 +0300

    user fix

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
+
 import { celebrate } from 'celebrate';
+
 import {
   getAllNotes,
   getNoteById,
@@ -7,16 +9,41 @@ import {
   deleteNote,
   updateNote,
 } from '../controllers/notesController.js';
+
 import {
   getAllNotesSchema,
   noteIdSchema,
   createNoteSchema,
   updateNoteSchema,
 } from '../validations/notesValidation.js';
+
+import { authenticate } from '../middleware/authenticate.js';
+
 const router = Router();
-router.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
-router.get('/notes/:noteId', celebrate(noteIdSchema), getNoteById);
-router.post('/notes', celebrate(createNoteSchema), createNote);
-router.delete('/notes/:noteId', celebrate(noteIdSchema), deleteNote);
-router.patch('/notes/:noteId', celebrate(updateNoteSchema), updateNote);
+
+router.get('/notes', authenticate, celebrate(getAllNotesSchema), getAllNotes);
+
+router.get(
+  '/notes/:noteId',
+  authenticate,
+  celebrate(noteIdSchema),
+  getNoteById,
+);
+
+router.post('/notes', authenticate, celebrate(createNoteSchema), createNote);
+
+router.delete(
+  '/notes/:noteId',
+  authenticate,
+  celebrate(noteIdSchema),
+  deleteNote,
+);
+
+router.patch(
+  '/notes/:noteId',
+  authenticate,
+  celebrate(updateNoteSchema),
+  updateNote,
+);
+
 export default router;
