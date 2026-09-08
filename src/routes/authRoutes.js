ad19fa7e3f5dbef09c55fbafee34bcd09c166ff3
oksana_martyniuk <mjlinkoln@gmail.com>
2026-09-08 22:15:16 +0300

    fix authRoutes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+
 import { celebrate, Segments } from 'celebrate';
 
 import {
@@ -16,7 +17,7 @@ import {
 const router = Router();
 
 router.post(
-  '/register',
+  '/auth/register',
   celebrate({
     [Segments.BODY]: registerUserSchema,
   }),
@@ -24,13 +25,15 @@ router.post(
 );
 
 router.post(
-  '/login',
+  '/auth/login',
   celebrate({
     [Segments.BODY]: loginUserSchema,
   }),
   loginUser,
 );
-router.post('/refresh', refreshUserSession);
-router.post('/logout', logoutUser);
+
+router.post('/auth/refresh', refreshUserSession);
+
+router.post('/auth/logout', logoutUser);
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 app.use(notesRoutes);
-app.use('/auth', authRoutes);
+app.use(authRoutes);
 app.use(notFoundHandler);
 app.use(errors());
 app.use(errorHandler);
